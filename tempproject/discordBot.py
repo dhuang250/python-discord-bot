38e6c5125fe08dd04bf9ba35659ec4f108257202
dhuang250 <116623618+dhuang250@users.noreply.github.com>
2022-11-08 22:06:29 -0800

    first commit
    still needs admin functions

---
+++ tempproject/discordBot.py
@@ -0,0 +1,74 @@
+import discord
+import random
+from discord.ext import commands
+from discord import TextChannel
+
+client = commands.Bot(command_prefix='$', case_insensitive=True)
+
+location = None
+
+destination = None
+
+
+# my talk command
+
+@client.command()
+async def rcv(ctx):
+    if ctx.author.id == 290928629192589312:
+        global location
+        location = ctx.channel
+        print(ctx.channel)
+    else:
+        await ctx.send("cant use this command. not dev of this bot")
+
+
+@client.command()
+async def say(ctx, *, msg=None):
+    print(location)
+    if ctx.author.id == 290928629192589312:
+        arr = msg.split()
+        if "@" in arr:
+            print("cant ping yet. need to add function")
+        ctx.channel = location
+        await ctx.channel.send(msg)
+    else:
+        await ctx.channel.send("cant use command. not dev of this bot")
+
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'{round(client.latency * 1000)} ms')
+
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("hello")
+
+
+
+@client.command()
+async def getUserID(ctx, *, user_id=None):
+    if user_id is not None:
+        print(user_id[3:-1])
+        await ctx.send("done")
+    else:
+        await ctx.send("member parameter required")
+
+
+@client.command()
+async def pinguser(ctx, *, user_id=None):
+    if user_id is not None:
+        member = await ctx.guild.fetch_member(user_id[3:-1])
+        await ctx.send(f'{member.mention}')
+    else:
+        await ctx.send("no member")
+
+
+client.run('<insert token>')
+
